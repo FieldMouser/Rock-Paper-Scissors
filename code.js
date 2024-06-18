e9febae35780b6230e7c11d9618b0f16579fdb1b
FieldMouser <vlad01.com98@gmail.com>
2024-06-18 12:17:39 +0700

    Add site-files

---
+++ code.js
@@ -0,0 +1,44 @@
+let playerChoice, computerChoice;
+let header = document.getElementById("result");
+
+function getComputerChoice() {
+    let choice = Math.random() * 3;
+
+    if (choice < 1) {
+        computerChoice = "Камень";
+    } else if (choice < 2) {
+        computerChoice = "Бумага";
+    } else {
+        computerChoice = "Ножницы";
+    }
+}
+
+function result() {
+    if (playerChoice === computerChoice) {
+        header.textContent = 'Ничья';
+    } else if ((playerChoice === "Бумага" && computerChoice === "Камень") ||
+               (playerChoice === "Ножницы" && computerChoice === "Бумага") ||
+               (playerChoice === "Камень" && computerChoice === "Ножницы")) {
+        header.textContent = 'Победа';
+    } else {
+        header.textContent = 'Поражение';
+    }
+}
+
+function playerChosenRock() {
+    playerChoice = "Камень";
+    getComputerChoice();
+    result();
+}
+
+function playerChosenPaper() {
+    playerChoice = "Бумага";
+    getComputerChoice();
+    result();
+}
+
+function playerChosenScissors() {
+    playerChoice = "Ножницы";
+    getComputerChoice();
+    result();
+}
